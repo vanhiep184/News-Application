43912b81177ea3b4f86e941dfd46e0191c797b49
Van Hiep <levanhiep.95.95@gmail.com>
2019-11-29 11:54:57 +0700

    Initial Commit

---
+++ News App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News App
+//
+//  Created by I Am Focused on 11/29/19.
+//  Copyright © 2019 LVHhcmus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
